da250e93ef658c3bc60b5ce272de3dffca585f1a
mhsMKulanathan <43574518+mhsMKulanathan@users.noreply.github.com>
2018-10-23 08:31:12 -0400

    Add files via upload

---
+++ p5/Mouse Part 2/sketch.js
@@ -0,0 +1,21 @@
+/* Using the mouse assignment part 1
+Madhusha Kulanathan
+17/10/2018*/
+
+//variable setup
+var r1=30;
+var r2=20;
+var r3= 10;
+var r4= 40;
+
+//page setup
+function setup() {
+createCanvas(600, 600);
+
+}
+
+// Draw line
+function draw() {
+background(250);
+line(mouseX,mouseY,r3,r1);
+  }
