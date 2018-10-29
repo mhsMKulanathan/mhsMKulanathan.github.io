6a7e874d7d5e33e96ecdbe067288d260eba4e217
mhsMKulanathan <43574518+mhsMKulanathan@users.noreply.github.com>
2018-10-29 09:04:01 -0400

    Update sketch.js

--- p5/Sound and Images/sketch.js
+++ p5/Sound and Images/sketch.js
@@ -2,12 +2,12 @@
 var mySound;
   function preload() {
     mySound = loadSound('The Nutcracker Dance Of The Sugar Plum Fairy.mp3');
-       image(Nutcracker.jpg,200,200,200,300);
+  
   }
 
   function setup() {
     createCanvas(640, 480);
-    // Notice that you have to put the variable name in front of the functions
+      image(Nutcracker.jpg,200,200,200,300);
     mySound.setVolume(50);
     mySound.play();
   }
