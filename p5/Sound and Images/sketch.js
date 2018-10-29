52fb6aab9074d155e891da01fc4a899ed02beaa4
mhsMKulanathan <43574518+mhsMKulanathan@users.noreply.github.com>
2018-10-29 09:05:33 -0400

    Update sketch.js

--- p5/Sound and Images/sketch.js
+++ p5/Sound and Images/sketch.js
@@ -2,12 +2,13 @@
 var mySound;
   function preload() {
     mySound = loadSound('The Nutcracker Dance Of The Sugar Plum Fairy.mp3');
-  
   }
 
   function setup() {
     createCanvas(640, 480);
-      image(Nutcracker.jpg,200,200,200,300);
     mySound.setVolume(50);
     mySound.play();
   }
+function draw () {
+   image(Nutcracker.jpg,200,200,200,300);
+}
